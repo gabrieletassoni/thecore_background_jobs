0e10b4a9b968af11db38e0ab05a7b0490b8767e4
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-10-10 09:21:22 +0200

    Added localized static links

--- config/initializers/thecore_background_jobs_rails_admin_config.rb
+++
@@ -1,6 +0,0 @@
-RailsAdmin.config do |config|
-  config.navigation_static_label = I18n.t("admin.settings.label")
-  config.navigation_static_links = {
-    "Sidekiq Dashboard" => "#{ENV['RAILS_RELATIVE_URL_ROOT']}/app/sidekiq"
-  }
-end
