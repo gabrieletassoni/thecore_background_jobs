5ed0833349f8e28e010e98783f271117025264c2
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-10-10 08:38:27 +0200

    Changed name to static link

--- config/initializers/thecore_background_jobs_rails_admin_config.rb
+++ config/initializers/thecore_background_jobs_rails_admin_config.rb
@@ -1,4 +1,5 @@
 RailsAdmin.config do |config|
+  config.navigation_static_label = I18n.t("admin.settings.label")
   config.navigation_static_links = {
     "Sidekiq Dashboard" => "#{ENV['RAILS_RELATIVE_URL_ROOT']}/app/sidekiq"
   }
