4ea645e7087d37db960538803f50c25544e3d6c3
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-02-12 12:14:30 +0000

    Remove failed sidekiq configs

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_admin_settings'
-
-Sidekiq.configure_server do |config|
-    config.redis = { url: Settings.ns(:sidekiq).redis_url }
-end
-
-Sidekiq.configure_client do |config|
-    config.redis = { url: Settings.ns(:sidekiq).redis_url }
-end
--- db/migrate/20210212112409_sidekiq_redis_url.rb
+++
@@ -1,5 +0,0 @@
-class SidekiqRedisUrl < ActiveRecord::Migration[6.0]
-  def change
-    Settings.ns(:sidekiq).redis_url = 'redis://127.0.0.1:6379/0'
-  end
-end
