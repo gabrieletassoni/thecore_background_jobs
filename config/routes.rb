51f225047f0faa63964a6c348267338e6ada9dcf
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-03-16 11:32:21 +0000

    Add Chomp to version and remove deprecated session secret from Sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
-  Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+  # Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
   authenticate do
     mount Sidekiq::Web, at: "/app/sidekiq"
   end
