bff8c5b1bde0eabb725c31030ccb9af300a8d70c
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-10-27 12:35:42 +0000

    Remove Redis url from Sidekiq

--- config/initializers/sidekiq-thecore.rb
+++ config/initializers/sidekiq-thecore.rb
@@ -1,10 +1,10 @@
-require 'sidekiq'
+# require 'sidekiq'
 
-current_db = rand(1..15)
+# current_db = rand(1..15)
 
-Sidekiq.configure_server do |config|
-    config.redis = { :namespace => "#{ENV['COMPOSE_PROJECT_NAME'] || 'notset'}", :url => "#{ENV['REDIS_URL']}/#{current_db}" }
-end
-Sidekiq.configure_client do |config|
-    config.redis = { :namespace => "#{ENV['COMPOSE_PROJECT_NAME'] || 'notset'}", :url => "#{ENV['REDIS_URL']}/#{current_db}" }
-end
+# Sidekiq.configure_server do |config|
+#     config.redis = { :namespace => "#{ENV['COMPOSE_PROJECT_NAME'] || 'notset'}", :url => "#{ENV['REDIS_URL']}/#{current_db}" }
+# end
+# Sidekiq.configure_client do |config|
+#     config.redis = { :namespace => "#{ENV['COMPOSE_PROJECT_NAME'] || 'notset'}", :url => "#{ENV['REDIS_URL']}/#{current_db}" }
+# end
