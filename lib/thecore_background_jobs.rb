30d27098911a9ca82037794847d6eb85af432c04
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-09-19 09:56:18 +0000

    rescue from redis timeouts

--- lib/thecore_background_jobs.rb
+++ lib/thecore_background_jobs.rb
@@ -21,29 +21,29 @@ module ThecoreBackgroundJobs
     #  "AlertForLongTermStockJob"=>{"cron"=>"0 0 9 * * *", "queue"=>"notset_default", "description"=>"This job Alerts a reference email for items in stock for too long", "enabled"=>true, "class"=>"AlertForLongTermStockJob"}, 
     #  "ScheduleEmployeeReminderForInStockParcelsJob"=>{"cron"=>"0 0 8,14 * * *", "queue"=>"notset_default", "description"=>"This job checks periodically for new received parcels", "enabled"=>true, "class"=>"ScheduleEmployeeReminderForInStockParcelsJob"}
     # }
-    Sidekiq.get_all_schedules.each_pair do |key, config|
-      puts "Key: #{key}, Config: #{config}"
-      schedule = "cron_for_#{key.underscore}"
-      setting = ThecoreSettings::Setting.where(ns: "schedules", key: schedule).pluck(:raw).first
-      puts "Setting is #{schedule} = #{setting}"
-      # Installing initial sidekiq.yml configuration if setting is not present
-      if setting.blank?
-        puts "Setting #{schedule} doesn't exist, creating based on current value: #{config["cron"]}"
-        ThecoreSettings::Setting.create(ns: "schedules", key: schedule, raw: config["cron"])
-        # Settings.ns("schedules").send("#{schedule}=", config["cron"])
-      elsif config["cron"].squeeze(" ").strip != setting.squeeze(" ").strip
-        # If we have a setting and is different from the currently loaded, then replace it 
-        # in scheduler configuration
-        puts "Setting #{schedule} exists: #{setting}"
-        begin
-          puts "Setting the schedule from DB value in Setting"
+    begin
+      Sidekiq.get_all_schedules.each_pair do |key, config|
+        puts "Key: #{key}, Config: #{config}"
+        schedule = "cron_for_#{key.underscore}"
+        setting = ThecoreSettings::Setting.where(ns: "schedules", key: schedule).pluck(:raw).first
+        puts "Setting is #{schedule} = #{setting}"
+        # Installing initial sidekiq.yml configuration if setting is not present
+        if setting.blank?
+          puts "Setting #{schedule} doesn't exist, creating based on current value: #{config["cron"]}"
+          ThecoreSettings::Setting.create(ns: "schedules", key: schedule, raw: config["cron"])
+          # Settings.ns("schedules").send("#{schedule}=", config["cron"])
+        elsif config["cron"].squeeze(" ").strip != setting.squeeze(" ").strip
+          # If we have a setting and is different from the currently loaded, then replace it 
+          # in scheduler configuration
+          puts "Setting #{schedule} exists: #{setting}"
           Sidekiq.set_schedule(key.underscore, { cron: setting.squeeze(" ").strip, queue: "#{ENV["COMPOSE_PROJECT_NAME"]}_default", class: key })
           puts "Reloading schedules"
           SidekiqScheduler::Scheduler.instance.reload_schedule!
-        rescue => exception
-          puts exception.message
         end
-      end
+      end 
+    rescue => exception
+      puts "Thecore Background Jobs: REDIS not reachable:"
+      puts exception.message
     end
     # check_in_stock_parcels = ThecoreSettings::Setting.where(ns: "schedules", key: "cron_for_check_in_stock_parcels").pluck(:raw).first
     # unless check_in_stock_parcels.blank?
