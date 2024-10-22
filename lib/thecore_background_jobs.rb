defd282e146bf3b646a858f6d1537d560c197c2a
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-10-22 11:27:37 +0000

    Use non cached version of SMTP config

--- lib/thecore_background_jobs.rb
+++ lib/thecore_background_jobs.rb
@@ -8,11 +8,25 @@ require "thecore_background_jobs/engine"
 module ThecoreBackgroundJobs
   # Your code goes here...
   def self.email_setup
-    delivery_options = { domain: Settings.ns(:smtp).domain, address: Settings.ns(:smtp).address, port: Settings.ns(:smtp).port.to_i, enable_starttls_auto: (Settings.ns(:smtp).enable_starttls_auto == "true")}
+    # Fill variables with the correct values
+    domain = ThecoreSettings::Setting.where(ns: :smtp, key: :domain).first.raw
+    address = ThecoreSettings::Setting.where(ns: :smtp, key: :address).first.raw
+    port = ThecoreSettings::Setting.where(ns: :smtp, key: :port).first.raw.to_i
+    enable_starttls_auto = ThecoreSettings::Setting.where(ns: :smtp, key: :enable_starttls_auto).first.raw == "true"
+    user_name = ThecoreSettings::Setting.where(ns: :smtp, key: :user_name).first.raw
+    password = ThecoreSettings::Setting.where(ns: :smtp, key: :password).first.raw
+    authentication = ThecoreSettings::Setting.where(ns: :smtp, key: :authentication).first.raw
+
+    delivery_options = { 
+      domain: domain, 
+      address: address, 
+      port: port, 
+      enable_starttls_auto: enable_starttls_auto
+    }
     # Other Options
-    delivery_options.merge!({user_name: Settings.ns(:smtp).user_name}) unless Settings.ns(:smtp).user_name.blank?
-    delivery_options.merge!({password: Settings.ns(:smtp).password}) unless Settings.ns(:smtp).password.blank?
-    delivery_options.merge!({authentication: Settings.ns(:smtp).authentication}) unless Settings.ns(:smtp).authentication.blank? 
+    delivery_options.merge!({user_name: user_name}) unless user_name.blank?
+    delivery_options.merge!({password: password}) unless password.blank?
+    delivery_options.merge!({authentication: authentication}) unless authentication.blank? || authentication == "none"
     delivery_options
   end
 
