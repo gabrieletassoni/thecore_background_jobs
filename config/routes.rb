e379b9c31acbb2d5a5ac6ab2cf88cab0d7d47120
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-17 16:41:22 +0000

    Step by step debug for sub-uri

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     
     # Allow any authenticated User with admin capability
     authenticate :user, lambda { |u| u.admin? } do
-        mount Sidekiq::Web => '/sidekiq', scope: ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/")
+        scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
+           mount Sidekiq::Web => "/sidekiq"
+        end
     end
 end
